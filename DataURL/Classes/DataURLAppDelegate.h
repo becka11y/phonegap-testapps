4a8df7f28785eea6982ddb868d8fb80bf3b9a38c
Becky Gibson <gibson.becky@gmail.com>
2011-04-14 16:37:14 -0400

    simple app for testing FileReader.readAsDataURL

---
+++ DataURL/Classes/DataURLAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  DataURLAppDelegate.h
+//  DataURL
+//
+//  Created by Becky Gibson on 4/14/11.
+//  Copyright IBM 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface DataURLAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
